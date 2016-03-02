a0c8e6755e3b92cc694f5aefe95a71f97a2b71cc
kaquilina <kennaquilina@gmail.com>
2016-03-02 23:49:15 +0100

    WorkSheet5
    Completed worksheet 5 on the code of worksheet4. Then copied the folder
    and renamed to worksheet5

---
+++ BSC Course/Lesson4/Lesson4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle myRectangle = new Rectangle();
+
+            foreach (object o in myRectangle.constructRectangle(10, 20))
+            { Console.WriteLine(o); }
+            Console.ReadLine();
+
+            //Console.Write("Enter the width: ");
+            //myRectangle.width = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter the heigth :");
+            //myRectangle.heigth = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Area is: " + myRectangle.WorkArea());
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ BSC Course/WorkSheet5/WorkSheet4/Computer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkSheet4
+{
+    class Computer
+    {
+        string _computerName;
+        string _computerStatus;
+        int _noOfFaults;
+
+        public string ComputerName
+        {
+            get
+            {
+                return _computerName;
+            }
+
+            set
+            {
+                _computerName = value;
+            }
+        }
+
+        public string ComputerStatus
+        {
+            get
+            {
+                return _computerStatus;
+            }
+
+            set
+            {
+                _computerStatus = value;
+            }
+        }
+
+        public int NoOfFaults
+        {
+            get
+            {
+                return _noOfFaults;
+            }
+
+            set
+            {
+                _noOfFaults = value;
+            }
+        }
+
+        public Computer(string computerName, string computerStatus, int noOfFaults)
+        {
+            ComputerName = computerName;
+            ComputerStatus = computerStatus;
+            NoOfFaults = noOfFaults;
+        }
+
+        override
+        public string ToString()
+        {
+            string output = "";
+            output =  "================================================\n";
+            output += " Computer Name : " + ComputerName + "\n";
+            output += " Computer Status : " + ComputerStatus + "\n";
+            output += " No of Faults : " + NoOfFaults + "\n";
+            output += "================================================\n";
+            return output;
+        }
+    }
+}
